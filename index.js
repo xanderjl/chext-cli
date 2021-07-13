75a89679905260f18ae30e78358e21100a12551f
Xander Low <alexanderjameslow@gmail.com>
2021-07-12 22:11:51 -0400

    added: installer automation and console output

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 const inquirer = require("inquirer");
 const degit = require("degit");
+const ora = require("ora");
+const { exec } = require("child_process");
 
 const cwd = process.cwd();
+const spinner = ora({ text: "Installing packages", color: "green" });
 
 inquirer
   .prompt([
@@ -19,8 +22,15 @@ inquirer
       choices: ["main", "sanity"],
       default: "main",
     },
+    {
+      type: "list",
+      message: "Select your package manager",
+      name: "pkgManager",
+      choices: ["npm", "yarn"],
+      default: "npm",
+    },
   ])
-  .then(({ projectName, starterBranch }) => {
+  .then(({ projectName, starterBranch, pkgManager }) => {
     let path = cwd + "/" + projectName;
     let branch = starterBranch;
 
@@ -34,12 +44,33 @@ inquirer
 
     emitter.clone(path).then(() => {
       console.log("done");
+
+      let installer = "npm install";
+      if (pkgManager === "yarn") {
+        installer = `yarn install`;
+      }
+      spinner.start();
+      exec(`cd ${projectName} && ${installer}`, (error, stdout, stderr) => {
+        spinner.stop();
+        console.log(stdout);
+        console.error(stderr);
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(
+            "\x1b[32m",
+            `You're all set! run cd ${projectName} && ${
+              pkgManager === "yarn" ? "yarn" : "npm run"
+            } dev`
+          );
+        }
+      });
     });
   })
   .catch((error) => {
     if (error.isTtyError) {
-      console.error("Oops. Something went wrong.")
+      console.error("Oops. Something went wrong.");
     } else {
-      console.error(`¯\\_(ツ)_/¯`)
+      console.error(`¯\\_(ツ)_/¯`);
     }
   });
